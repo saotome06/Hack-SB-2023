03a20ed63c552baee3121ed7db360054917f9644
Kahuka0506 <kou22uhneiom@icloud.com>
2023-09-23 20:35:36 +0900

    create image

--- pages/openai_api_test.tsx
+++ pages/openai_api_test.tsx
@@ -74,6 +74,7 @@ export default function Home() {
         dangerouslyAllowBrowser: true,
       });
       console.log("start");
+      console.log(process.env.NEXT_PUBLIC_OPENAI_API_KEY);
 
       const p = Math.random();
       let prompt_base = "";
