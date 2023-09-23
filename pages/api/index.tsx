90bb694ece5446380fa579b02caf1078af71f7f4
kazuhiko <sirius.nasa1000@gmail.com>
2023-09-23 14:48:00 +0900

    latest

--- pages/api/index.tsx
+++ pages/api/index.tsx
@@ -1,17 +1,15 @@
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 
 export const config = {
   api: {
-    bodyParser: false,
+    bodyParser: true,
   },
 };
 
 export default function handler(req, res) {
-  if (req.method === 'POST') {
-    const data = req.body;
-    res.status(200).json({ name: data.name });
-  } else {
-    res.status(405).end();
+  console.log("受付開始");
+  if (req.method === "POST") {
+    return res.status(200).json({ message: "OK" });
   }
 }
 
