ebde4db483fa8d2ab9ebd5b33d12e6e79154b963
Kahuka0506 <kou22uhneiom@icloud.com>
2023-09-23 18:31:55 +0900

    card name

--- pages/openai_api_test.tsx
+++ pages/openai_api_test.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   function Attack_Name_Button() {
     const [name, setname] = useState("ちょうすごいパーンチ");
     const [attack_score_by_name, set_attack_score_by_name] = useState(1000);
-    //const [attack_score, set_attack_score] = useState(1000);
+    const [card_name, set_card_name] = useState("超すごい人");
     const [output_data, set_randomData] = useState(
       "「ちょーすごいパーンチ」は、使用者が蓄積した集中力とエネルギーを一点に絞り、極限まで強化した拳を一瞬で放つ技。力の源は真剣勝負の熱量で、パンチが命中すれば周囲も巻き込むほどの衝撃波を生む。",
     );
@@ -88,7 +88,7 @@ export default function Home() {
       console.log(p);
       setIsLoadingText(true);
       setIsLoadingAttackScore(true);
-      const content = prompt_base + "技名: " + prompt + "";
+      const content = prompt_base + "必殺技名: " + prompt + "";
       const completion = await openai.chat.completions.create({
         messages: [{ role: "user", content: content }],
         model: "gpt-4",
@@ -98,8 +98,29 @@ export default function Home() {
       const answer = completion.choices[0].message?.content;
       console.log(answer);
       setIsLoadingText(false);
-      console.log("end");
       set_randomData(answer);
+
+      const content1 =
+        "必殺技名，必殺技の説明，顔写真の画像の情報があります．これらの情報の特徴をよく表したセンスあるいい感じの名前を考えてください．名前は5文字以上20文字以内です．返答は名前のみ返してください．使う情報は以下のとおりです\n" +
+        "必殺技名: " +
+        prompt +
+        "，必殺技の説明: " +
+        answer;
+      const completion1 = await openai.chat.completions.create({
+        messages: [{ role: "user", content: content1 }],
+        model: "gpt-4",
+      });
+      const answer1 = completion1.choices[0].message?.content;
+      answer1
+        .replace("[", "")
+        .replace("]", "")
+        .replace('"', "")
+        .replace("「", "")
+        .replace("」", "");
+      console.log(answer1);
+      set_card_name(answer1);
+
+      console.log("end");
     }
 
     async function sendPrompt_cal_attack_score(prompt = "") {
@@ -160,6 +181,7 @@ export default function Home() {
     return (
       <>
         <form>
+          <h1 className="text-5xl">card name : {card_name}</h1>
           <div className="text-2xl">
             face mesh:
             <input
@@ -241,22 +263,35 @@ export default function Home() {
           <p>Loading...</p>
         ) : (
           <p className="text-3xl font-bold underline">
-            attack score by name : {attack_score_by_name}
+            attack score by name :{" "}
+            <font color="blue" size="5">
+              {attack_score_by_name}
+            </font>
           </p>
         )}
 
         {isLoadingText ? (
           <p>Loading...</p>
         ) : (
-          <p className="text-3xl font-bold underline">{output_data}</p>
+          <p className="text-3xl font-bold underline">
+            <font color="black" size="6">
+              {output_data}
+            </font>
+          </p>
         )}
         <p className="text-3xl font-bold underline">face pos : {face_pos}</p>
         <p className="text-3xl font-bold underline">
-          smile score : {smile_score}
+          smile score :{" "}
+          <font color="blue" size="5">
+            {smile_score}
+          </font>
         </p>
 
         <p className="text-4xl font-bold underline">
-          attack score : {attack_score_by_name * 0.7 + smile_score * 0.3}
+          attack score :{" "}
+          <font color="red" size="5">
+            {attack_score_by_name * 0.7 + smile_score * 0.3}
+          </font>
         </p>
       </>
     );
@@ -264,7 +299,6 @@ export default function Home() {
 
   return (
     <main>
-      <h1 className="text-5xl">Name</h1>
       <Attack_Name_Button />
     </main>
   );
