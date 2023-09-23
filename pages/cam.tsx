26bbfa2262f43a8b73f6c7b1f8168a0a0f056cf4
TANUKIpro <ryotaro_koike@outlook.com>
2023-09-23 12:17:45 +0900

    iOSのカメラに対応

--- pages/cam.tsx
+++ pages/cam.tsx
@@ -23,138 +23,7 @@ if (typeof window !== "undefined") {
 //   return Array.from({ length: end - start }, (_, i) => start + i);
 // }
 
-const base_point = 6;
-// const left_eye_indices = range(33, 46);
-// const right_eye_indices = range(263, 276);
-// const mouth_indices = [...range(0, 17), ...range(61, 69)];
-// const eyebrow_indices = [...range(17, 33), ...range(246, 263)];
-
-// const __FACEMESH_CONTOURS = [
-//   [178, 87],
-//   [181, 84],
-//   [82, 13],
-//   [283, 282],
-//   [136, 172],
-//   [381, 382],
-//   [105, 66],
-//   [311, 310],
-//   [70, 63],
-//   [300, 293],
-//   [87, 14],
-//   [39, 37],
-//   [382, 362],
-//   [145, 153],
-//   [160, 159],
-//   [251, 389],
-//   [282, 295],
-//   [159, 158],
-//   [296, 336],
-//   [334, 296],
-//   [148, 176],
-//   [80, 81],
-//   [144, 145],
-//   [67, 109],
-//   [52, 65],
-//   [390, 373],
-//   [263, 249],
-//   [153, 154],
-//   [46, 53],
-//   [374, 380],
-//   [246, 161],
-//   [310, 415],
-//   [276, 283],
-//   [267, 269],
-//   [387, 386],
-//   [61, 146],
-//   [317, 402],
-//   [78, 191],
-//   [380, 381],
-//   [377, 152],
-//   [191, 80],
-//   [249, 390],
-//   [466, 388],
-//   [158, 157],
-//   [293, 334],
-//   [375, 291],
-//   [66, 107],
-//   [93, 234],
-//   [37, 0],
-//   [163, 144],
-//   [400, 377],
-//   [84, 17],
-//   [314, 405],
-//   [13, 312],
-//   [373, 374],
-//   [270, 409],
-//   [365, 379],
-//   [157, 173],
-//   [312, 311],
-//   [318, 324],
-//   [378, 400],
-//   [324, 308],
-//   [332, 284],
-//   [356, 454],
-//   [63, 105],
-//   [21, 54],
-//   [384, 398],
-//   [162, 21],
-//   [288, 397],
-//   [173, 133],
-//   [263, 466],
-//   [386, 385],
-//   [33, 7],
-//   [61, 185],
-//   [161, 160],
-//   [132, 93],
-//   [103, 67],
-//   [53, 52],
-//   [78, 95],
-//   [91, 181],
-//   [321, 375],
-//   [454, 323],
-//   [0, 267],
-//   [323, 361],
-//   [176, 149],
-//   [155, 133],
-//   [409, 291],
-//   [295, 285],
-//   [109, 10],
-//   [14, 317],
-//   [234, 127],
-//   [379, 378],
-//   [88, 178],
-//   [95, 88],
-//   [40, 39],
-//   [284, 251],
-//   [17, 314],
-//   [415, 308],
-//   [185, 40],
-//   [385, 384],
-//   [338, 297],
-//   [297, 332],
-//   [152, 148],
-//   [81, 82],
-//   [7, 163],
-//   [389, 356],
-//   [398, 362],
-//   [154, 155],
-//   [33, 246],
-//   [361, 288],
-//   [65, 55],
-//   [269, 270],
-//   [405, 321],
-//   [397, 365],
-//   [149, 150],
-//   [146, 91],
-//   [172, 58],
-//   [10, 338],
-//   [54, 103],
-//   [150, 136],
-//   [388, 387],
-//   [58, 132],
-//   [127, 162],
-//   [402, 318],
-// ];
+const REF_POINT = 6;
 
 const MOUTH = [
   [80, 81],
@@ -301,8 +170,8 @@ export default function FaceMesher() {
           const ctx = canvasRef.current.getContext("2d");
 
           ctx.arc(
-            landmarks[base_point].x * canvas.width,
-            landmarks[base_point].y * canvas.height,
+            landmarks[REF_POINT].x * canvas.width,
+            landmarks[REF_POINT].y * canvas.height,
             2,
             0,
             2 * Math.PI,
@@ -311,7 +180,6 @@ export default function FaceMesher() {
           ctx.fill();
 
           // 各インデックス配列をループして特徴点を描画
-          // for (const index of [...left_eye_indices, ...right_eye_indices, ...mouth_indices, ...eyebrow_indices]) {
           for (const index of [
             ...MOUTH_INDEX,
             ...L_EYE_INDEX,
@@ -321,7 +189,6 @@ export default function FaceMesher() {
           ]) {
             const point = landmarks[index];
             ctx.beginPath();
-            //ctx.arc(point.x, point.y, 2, 0, 2 * Math.PI);
             ctx.arc(
               point.x * canvas.width,
               point.y * canvas.height,
@@ -336,27 +203,6 @@ export default function FaceMesher() {
       }
     });
 
-    // faceMesh.onResults((results: { multiFaceLandmarks: any; }) => {
-    //   if (results.multiFaceLandmarks) {
-    //     for (const landmarks of results.multiFaceLandmarks) {
-    //       const ctx = canvasRef.current.getContext('2d');
-
-    //       // ペアごとにランドマークをループして線で結びます
-    //       for (const pair of FACEMESH_CONTOURS) {
-    //         const start = landmarks[pair[0]];
-    //         const end = landmarks[pair[1]];
-
-    //         ctx.beginPath();
-    //         ctx.moveTo(start.x * canvas.width, start.y * canvas.height);
-    //         ctx.lineTo(end.x * canvas.width, end.y * canvas.height);
-    //         ctx.strokeStyle = '#FF0000';
-    //         ctx.lineWidth = 1;
-    //         ctx.stroke();
-    //       }
-    //     }
-    //   }
-    // });
-
     try {
       await faceMesh.send({ image: canvas });
     } catch (error) {
@@ -367,7 +213,15 @@ export default function FaceMesher() {
   return (
     <div>
       <button onClick={startCamera}>カメラを起動</button>
-      <video ref={videoRef} width="600" height="400" autoPlay></video>
+      <video
+        ref={videoRef}
+        width="600"
+        height="400"
+        id="video"
+        autoPlay
+        muted
+        playsInline
+      ></video>
       <button onClick={processCameraFrame}>写真を撮る</button>
       <canvas ref={canvasRef} id="output"></canvas>
     </div>
