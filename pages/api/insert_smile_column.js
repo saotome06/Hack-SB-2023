5675020ac9d0ad00efe87f076bca0bf618e43356
kazuhiko <sirius.nasa1000@gmail.com>
2023-09-24 03:21:13 +0900

    405排除

--- pages/api/insert_smile_column.js
+++ pages/api/insert_smile_column.js
@@ -6,24 +6,20 @@ const supabase = createClient(
 );
 // supabaseにデータを挿入する
 export default async function insertHandler(req, res) {
-  if (req.method === "POST") {
-    const { data, error } = await supabase.from("smile_cards").insert([
-      {
-        smile_score: req.body.smile_score,
-        card_name: req.body.card_name,
-        special_attack_name: req.body.special_attack_name,
-        description: req.body.description,
-        attack_power: req.body.attack_power,
-        background_url: req.body.background_url,
-        face_image_path: req.body.face_image_path,
-      },
-    ]);
-    if (error) {
-      res.status(500).json({ error: error.message });
-    } else {
-      res.status(200).json({ data });
-    }
+  const { data, error } = await supabase.from("smile_cards").insert([
+    {
+      smile_score: req.body.smile_score,
+      card_name: req.body.card_name,
+      special_attack_name: req.body.special_attack_name,
+      description: req.body.description,
+      attack_power: req.body.attack_power,
+      background_url: req.body.background_url,
+      face_image_path: req.body.face_image_path,
+    },
+  ]);
+  if (error) {
+    res.status(500).json({ error: error.message });
   } else {
-    res.status(405).json({ error: "Method not allowed" });
+    res.status(200).json({ data });
   }
 }
