a6b6a0b34e284a3b698841e9b84517aaa09ec9a3
kazuhiko <sirius.nasa1000@gmail.com>
2023-09-24 03:26:04 +0900

    もとに戻す

--- pages/api/insert_smile_column.js
+++ pages/api/insert_smile_column.js
@@ -6,20 +6,25 @@ const supabase = createClient(
 );
 // supabaseにデータを挿入する
 export default async function insertHandler(req, res) {
-  const { data, error } = await supabase.from("smile_cards").insert([
-    {
-      smile_score: req.body.smile_score,
-      card_name: req.body.card_name,
-      special_attack_name: req.body.special_attack_name,
-      description: req.body.description,
-      attack_power: req.body.attack_power,
-      background_url: req.body.background_url,
-      face_image_path: req.body.face_image_path,
-    },
-  ]);
-  if (error) {
-    res.status(500).json({ error: error.message });
+  if (req.method === "POST") {
+    const { data, error } = await supabase.from("smile_cards").insert([
+      {
+        smile_score: req.body.smile_score,
+        card_name: req.body.card_name,
+        special_attack_name: req.body.special_attack_name,
+        description: req.body.description,
+        attack_power: req.body.attack_power,
+        background_url: req.body.background_url,
+        face_image_path: req.body.face_image_path,
+      },
+    ]);
+    if (error) {
+      res.status(500).json({ error: error.message });
+    } else {
+      res.status(200).json({ data });
+    }
   } else {
-    res.status(200).json({ data });
+    console.log(data);
+    res.status(405).json({ error: "Method not allowed" });
   }
 }
